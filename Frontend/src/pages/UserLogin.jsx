4929f7dea0ca46cd51e873e4d511cbbe547935fa
Pranjal Sharma <pranjalsharma9143@gmail.com>
2025-11-27 15:09:43 +0530

    login api change

--- Frontend/src/pages/UserLogin.jsx
+++ Frontend/src/pages/UserLogin.jsx
@@ -16,7 +16,7 @@ const UserLogin = () => {
             email: email,
             password: password
         }
-        const response = await axios.post('https://foodreels-yriy.onrender.com/api/auth/user/login', userLogin, { withCredentials: true });
+        const response = await axios.post(`${import.meta.env.VITE_BASE_URL}/api/auth/user/login`, userLogin, { withCredentials: true });
         if (response.status === 200) {
             const data = response.data
             setUserData(data.user);
